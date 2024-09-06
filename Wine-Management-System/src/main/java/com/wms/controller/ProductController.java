302316983b4cb408f11b5918be2506479d0e7dca
praful.kasare <prafulkasare16@gmail.com>
2024-09-06 11:19:16 +0530

    Make ProductController as Cross Origin

--- Wine-Management-System/src/main/java/com/wms/controller/ProductController.java
+++ Wine-Management-System/src/main/java/com/wms/controller/ProductController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("**")
 public class ProductController {
 
     Logger logger = LoggerFactory.getLogger(ProductController.class);
